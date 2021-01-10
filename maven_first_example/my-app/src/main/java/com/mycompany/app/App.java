d465b47b9fb93c686dbaf3a8e12d6f929dc07323
tejprakash1 <38498838+tejprakash1@users.noreply.github.com>
2021-01-10 11:02:08 +0530

    Update App.java

--- maven_first_example/my-app/src/main/java/com/mycompany/app/App.java
+++ maven_first_example/my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World....Good morning!" );
+        System.out.println( "Hello World....Good morning!...Good afternoon!!" );
     }
 }
